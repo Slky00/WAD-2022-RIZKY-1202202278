dd9aeb5b19dca3f1dc158c347f6d2e806034386e
Rizky <rizkybernawan@gmail.com>
2022-11-30 08:12:07 +0700

    Debuging modul 4

--- MODUL 4 RIZKY/config/register.php
+++ MODUL 4 RIZKY/config/register.php
@@ -18,10 +18,10 @@ if($rows) {
         $query = mysqli_query ($connects, "INSERT INTO user_rizky (email, nama, no_hp, password) VALUES ('$emailu', '$nama', '$nohp','$password')");
 
         if($query) {
-            setcokie('nama', $nama, time() +85400*60, '/');
-            setcokie('email', $emailu, time() +85400*60, '/');
-            setcokie('password', $password, time() +85400*60, '/');
-            setcokie('no_hp', $nohp, time() +85400*60, '/');
+            setcookie('nama', $nama, time() +85400*60, '/');
+            setcookie('email', $emailu, time() +85400*60, '/');
+            setcookie('password', $password, time() +85400*60, '/');
+            setcookie('no_hp', $nohp, time() +85400*60, '/');
             header("Location: ../Rizky_index.php?page=login");
         }
     } else {
